08a37f2c44edfd295209771a7fc6efd23d0128d8
Hadj H. <elhadjorg@gmail.com>
2025-05-19 18:24:23 +0100

    Remove redundant log statement for loading environment variables

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const { Client } = await import("@langchain/langgraph-sdk");
 if (process.env.NODE_ENV !== "production") {
-    console.log("Loading environment variables from .env file...");
     const dotenv = await import("dotenv");
     dotenv.config();
 }
 
 const client = new Client({ apiUrl: process.env.LANGGRAPH_API_URL });
-
+console.log("Connecting to: ", process.env.LANGGRAPH_API_URL);
 const response = await client.runs.wait(
     null, // Threadless run
     "agent", // Assistant ID
